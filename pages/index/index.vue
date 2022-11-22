e31484784c221eb85f104d5995fadf6d93e587a7
ende151@163.com <ende151@163.com>
2022-11-22 09:18:28 +0800

    项目上传

---
+++ pages/index/index.vue
@@ -0,0 +1,67 @@
+<template>
+	<view class="content">
+		<!-- <image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view> -->
+		<u--input placeholder="请输入内容" border="surround" v-model="value" @change="change"></u--input>
+	</view>
+</template>
+
+<script>
+
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				list: [{
+					text: '点赞',
+					color: 'blue',
+					fontSize: 28
+				}, {
+					text: '分享'
+				}, {
+					text: '评论'
+				}],
+				show: true,
+				value: '我是'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			change() {
+				console.log('111')
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
